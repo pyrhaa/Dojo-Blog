b659cfd4c80e6dace0dd8deeb0490d257a73ee22
pyrhaa <sboubtane@protonmail.com>
2021-05-01 18:52:28 +0200

    databaseJson and handle errors

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3
-    }
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  //simulate a time loading when fetching data from a database
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('could not fetch the data fot that resource');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((e) => {
+          setIsPending(false);
+          setError(e.message);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All Blogs!' />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
     </div>
   );
 };
